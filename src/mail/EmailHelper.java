84d1e811dd908a480f141be5a52629ef4283113a
Tanh Nguyen <tanhnv.dev@gmail.com>
2020-06-28 15:29:30 +0700

    adding EmailHelper to send email when error

---
+++ src/mail/EmailHelper.java
@@ -0,0 +1,61 @@
+package mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailHelper extends Thread {
+	private static String mail = "nvtanh4vipm@gmail.com";
+	private static String pass = "vipmember";
+	private String to, subject, body;
+
+	public EmailHelper(String to, String subject, String body) {
+		this.to = to;
+		this.subject = subject;
+		this.body = body;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		sendMail();
+	}
+	
+	private void sendMail() {
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(mail, pass);
+			}
+		});
+
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setHeader("Content-Type", "text/plain; charset=UTF-8");
+			message.setFrom(new InternetAddress("dangvanda.itnlu@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject(this.subject, "UTF-8");
+			message.setText(this.body, "UTF-8");
+			Transport.send(message);
+			System.out.println("message sent successfully...");
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
